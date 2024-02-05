06a414860ddabd9c0aca27df0beb0632bd770c54
Neto test <akmalova.elza@yandex.ru>
2024-02-05 19:38:07 +0300

    src added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,11 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCalculateCashBack {
+        CashbackHackService = new CashbackHackService();
+
+    }
+}
