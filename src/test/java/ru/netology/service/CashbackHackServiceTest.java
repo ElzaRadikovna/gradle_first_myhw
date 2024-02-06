9c49b408e48a25ed2237d7db97dd62397f139a5e
Neto test <akmalova.elza@yandex.ru>
2024-02-06 11:01:00 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    void cashbackIfOver1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int actual = cashbackHackService.remain(1600);
+        int expected = 400;
+
+        Assert.assertEquals(actual, expected);
+
+
+    }
+    @Test
+    void cashbackIfUnder1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int actual = cashbackHackService.remain(850);
+        int expected = 150;
+
+        Assert.assertEquals(actual, expected);
+
+
+    }
+    @Test
+    void cashbackIfEqual1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+
+
+
+    }
+}
